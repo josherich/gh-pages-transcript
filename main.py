4aa4aacb90f303fd4e1efe476883eef82dfdcc99
josh chen <josherichchen@gmail.com>
2025-02-19 17:58:12 -0500

    add new episode

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from env import *
+from datetime import datetime
+import uuid
 
 from fasthtml.common import *
 from fastcore.utils import *
@@ -16,13 +18,16 @@ def start_queue():
 
 EPISODES_FILE = "queue.json"
 
-def load_episodes():
+def load_episodes(status = None):
     episodes = []
     with open(EPISODES_FILE, "r") as f:
         episodes = json.load(f)
 
     # sort by published date
+    episodes = [{'id': i, **ep} for i, ep in enumerate(episodes)]
     episodes.sort(key=lambda x: x['published_date'], reverse=True)
+    if status:
+        episodes = [ep for ep in episodes if ep['status'] == status]
     return episodes
 
 def save_episodes(data):
@@ -44,6 +49,7 @@ def episode_form(i, ep):
                     Option("todo",  value="todo",  selected=(ep["status"]=="todo")),
                     Option("queued",value="queued",selected=(ep["status"]=="queued")),
                     Option("done",  value="done",  selected=(ep["status"]=="done")),
+                    Option("skip", value="skip", selected=(ep["status"]=="skip")),
                     name="status"
                 ),
                 style="margin: 0.5rem 0;"
@@ -62,11 +68,43 @@ def episode_form(i, ep):
         style="margin-bottom:1rem; padding:1rem; border:1px solid #ccc;"
     )
 
+def new_episode_form():
+    return Card(
+        Form(
+            Div(
+                "Title: ",
+                Input(type="text", name="title"),
+                style="margin-bottom: 0.5rem;"
+            ),
+            Div(
+                "URL: ",
+                Input(type="text", name="url"),
+                style="margin-bottom: 0.5rem;"
+            ),
+            Button("Submit"),
+            hx_post="/new", hx_swap="none", hx_target=f"#episodes"
+        ),
+        id=f"episode-new",
+        style="margin-bottom:1rem; padding:1rem; border:1px solid #ccc;"
+    )
+
 @rt("/")
-def get():
-    episodes = load_episodes()
-    forms = [episode_form(i, ep) for i, ep in enumerate(episodes)]
-    return Container(Button('Pull History', hx_post='/pull', style='width: 100%; margin:1em 0; padding: 1em 0;'), *forms, style="max-width:800px; margin:auto; padding:1rem;")
+def get(status: str = 'todo'):
+    episodes = load_episodes(status)
+    forms = [episode_form(ep['id'], ep) for ep in episodes]
+    return Container(
+        Span(
+            A(B("todo") if status == 'todo' else 'todo', href="/?status=todo"), " | ",
+            A(B("queued") if status == 'queued' else 'queued', href="/?status=queued"), " | ",
+            A(B("done") if status == 'done' else 'done', href="/?status=done"), " | ",
+            A(B("error") if status == 'error' else 'error', href="/?status=error"), " | ",
+            A(B("skip") if status == 'skip' else 'skip', href="/?status=skip"),
+        ),
+        Button('Pull History', hx_post='/pull', style='width: 100%; margin:1em 0; padding: 0.5em 0;'),
+        new_episode_form(),
+        Div(*forms, id="episodes"),
+        style="max-width:800px; margin:auto; padding:1rem;"
+    )
 
 @rt("/update")
 def post(id: int, status: str, url: str):
@@ -75,6 +113,20 @@ def post(id: int, status: str, url: str):
     episodes[id]["url"] = url
     save_episodes(episodes)
     return episode_form(id, episodes[id])
+
+@rt("/new")
+def post(title: str, url: str):
+    episodes = load_episodes()
+    episodes.insert(0, {
+        "id": uuid.uuid4().hex,
+        "title": title,
+        "url": url,
+        "type": "youtube" if "youtube" in url else "podcast",
+        "status": "todo",
+        "published_date": datetime.now().strftime("%Y-%m-%d")
+    })
+    save_episodes(episodes)
+    return RedirectResponse('/', status_code=303)
 
 @rt("/pull")
 def post():
